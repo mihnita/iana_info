2a5e51b20d32f40a0257f6ef85041685c0327514
Mihai Nita <mnita@google.com>
2023-12-07 13:19:53 -0800

    Update to Unicode 15.1

--- getdata.sh
+++ getdata.sh
@@ -6,8 +6,8 @@ pushd udata
 
 wget https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
 
-wget ftp://unicode.org/Public/15.0.0/ucd/Blocks.txt
-wget ftp://unicode.org/Public/15.0.0/ucd/NamesList.txt
-wget ftp://unicode.org/Public/15.0.0/ucd/UnicodeData.txt
+wget ftp://unicode.org/Public/15.1.0/ucd/Blocks.txt
+wget ftp://unicode.org/Public/15.1.0/ucd/NamesList.txt
+wget ftp://unicode.org/Public/15.1.0/ucd/UnicodeData.txt
 
 popd
